d5a36d02334faa74c044d63624fb5d8a3bc79d14
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-24 20:24:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-  #my_new_array = []
-  #array.each do | number |
-    #number = number ** 2
-    #my_new_array << number
-  #end
-  #my_new_array
-  array.collect {|number| number ** 2}
+  my_new_array = []
+  array.each do | number |
+    number = number ** 2
+    my_new_array << number
+  end
+  my_new_array
+  #array.collect {|number| number ** 2}
 end
